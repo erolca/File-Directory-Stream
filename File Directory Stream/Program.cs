fdc17a0c1e7b3621a1fc49b8394ac16e7b97e46f
erolcalisgan <erolcalisgan@hotmail.com>
2018-02-09 10:49:44 +0300

    Add project files.

---
+++ File Directory Stream/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace File_Directory_Stream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
